01fd1c2bacc1b94e639b385ddd3de3f6638987f2
Maisa Ahmad <2013maisaah@gmail.com>
2020-10-24 20:31:48 -0400

    outlined webpages

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', function (){
-  document.querySelector('button').addEventListener('click',onclick, false)
+  document.querySelector('#filter').addEventListener('click',onclick, false)
   function onclick(){
     chrome.tabs.query({currentWindow: true, active: true},
       function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, 'Sensitive material detected', setCount)
       })
   }
+  document.querySelector('#site-link').addEventListener('click',onClick, false)
+  function onClick(){
+    var newURL = "https://maisa-ah.github.io/islamophobia_detector/"
+    chrome.tabs.create({ url: newURL })
+  }
 
   function setCount(res){
     const div = document.getElementById('detection')
     const img = document.getElementById('detect-img')
-    if(res.count){
-      div.textContent =  'Sensitive content found'
+    if(res.count>0){
+      div.textContent = 'Sensitive content found'
       img.src="images/warning.png"
     }
   }
